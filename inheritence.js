f6e5eb72215fd4bb75cc026eb94505061a94161f
Shivam Sharma <120860468+Shiva12k4@users.noreply.github.com>
2024-12-04 12:59:51 +0000

    js

--- inheritence.js
+++ inheritence.js
@@ -24,3 +24,18 @@ addCourse(){
 
 const tea= new Teacher('chai','tea@gmail.com',1234);
 tea.addCourse();
+
+
+const tea2= new User('chai2');
+
+// tea2.addCourse(); tea2 does not have access of addcourse
+tea2.logMe(); // tea2 have access of logme
+
+console.log(tea===tea2);//false
+console.log(tea === Teacher);//false
+console.log(tea instanceof Teacher);//true
+
+//JavaScript mein, instance ka matlab hai ek aisa object(tea) jo kisi class(teacher) ya constructor function ke through banaya gaya ho. Agar ek object kisi specific class ka "example" ya "instance" hai, to uska matlab hai ki wo us class ke properties aur methods ko inherit karta hai.
+
+
+
